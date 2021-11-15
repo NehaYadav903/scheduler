7387ace2ec102435233ba245949f0c63c36fc1e1
Neha Yadav <nehayadav903@gmail.com>
2021-11-15 15:01:14 -0500

    create Appointment component that will hold a number of child components

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,29 @@
+import React from "react" ;
+import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
+
+ // to show list of interviewers
+ export default function InterviewerList(props) {
+
+ const interviewers = props.interviewers.map(interviewer => {
+  return (
+    <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={() => props.setInterviewer(interviewer.id)}
+    />
+  );
+});
+return (
+  <li className={interviewers} onClick={props.setInterviewer}>
+    <img
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
+    />
+    {props.selected && props.name}
+  </li>
+);
+ }
