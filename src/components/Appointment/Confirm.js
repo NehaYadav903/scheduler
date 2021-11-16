b8e18ca35a490310d8727f0acaf8fb4091f01b43
Neha Yadav <nehayadav903@gmail.com>
2021-11-16 00:36:56 -0500

    Update it the component with props to being passed

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,12 +1,19 @@
 import React from "react";
 
+import "./styles.scss";
+import Button from "components/Button";
+
 export default function Empty(props) {
   return (
     <main className="appointment__card appointment__card--confirm">
       <h1 className="text--semi-bold">Delete the appointment?</h1>
       <section className="appointment__actions">
-        <Button danger>Cancel</Button>
-        <Button danger>Confirm</Button>
+        <Button danger onClick={props.onCancel}>
+          Cancel
+        </Button>
+        <Button danger onClick={props.onConfirm}>
+          Confirm
+        </Button>
       </section>
     </main>
   );
