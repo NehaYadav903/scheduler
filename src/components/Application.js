93e8d575cae50c4f41f087641b0ce5ebf3d0532f
Neha Yadav <nehayadav903@gmail.com>
2021-11-18 01:21:26 -0500

    add proxy to work with scheduler api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,54 +1,58 @@
 import React, { useState } from "react";
 
 import "components/Application.scss";
-import DayList from "components/DayList";
-import Appointment from"components/Appointment"; 
+import DayList from "components/DayList.js";
+import Appointment from "components/Appointment";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+
+  const appointments = {
+    "1": {
+      id: 1,
+      time: "12pm",
+    },
+    "2": {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer: {
+          id: 3,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    },
+    "3": {
+      id: 3,
+      time: "2pm",
+    },
+    "4": {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Archie Andrews",
+        interviewer: {
+          id: 4,
+          name: "Cohana Roy",
+          avatar: "https://i.imgur.com/FK8V841.jpg",
+        }
+      }
+    },
+    "5": {
+      id: 5,
+      time: "4pm",
+    }
+  };
+
+  const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
           className="sidebar--centered"
           src="images/logo.png"
@@ -61,18 +65,21 @@ export default function Application(props) {
             value={day}
             onChange={setDay}
           />
+
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
-
       </section>
       <section className="schedule">
-        {Object.values(appointments).map()}
+       
+        
         <Appointment key="last" time="5pm" />
+
       </section>
     </main>
   );
+
 }
