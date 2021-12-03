e5473b5714b6b642740d98477c77d351bb4a0829
Neha Yadav <nehayadav903@gmail.com>
2021-12-03 15:19:42 -0500

    Refactor the code

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -13,7 +13,7 @@ import { render } from "@testing-library/react";
   We import the component that we are testing
 */
 import Appointment from "components/Appointment";
-
+import Application from "components/Application";
 /*
   A test that renders a React Component
 */
@@ -24,11 +24,4 @@ describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
   });
-it("does something it is supposed to do", () => {
-  // ...
-});
-
-it("does something else it is supposed to do", () => {
-  // ...
-});
 });
