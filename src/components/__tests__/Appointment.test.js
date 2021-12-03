a75b272178d785e25063e1f983438385774b13ae
Neha Yadav <nehayadav903@gmail.com>
2021-12-03 15:14:43 -0500

    Refactor the code

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -17,6 +17,9 @@ import Appointment from "components/Appointment";
 /*
   A test that renders a React Component
 */
+it("renders without crashing", () => {
+  render(<Application />);
+});
 describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
