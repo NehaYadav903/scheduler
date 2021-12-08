492aa11820c007fceb6fe24602ca53c28e5a073c
Neha Yadav <nehayadav903@gmail.com>
2021-12-08 13:25:51 -0500

    Use the raw Promise syntax to the newer async/await syntax

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,17 +6,16 @@ import {
   fireEvent,
 }
   from "@testing-library/react";
-
 import Application from "components/Application";
 
 afterEach(cleanup);
+describe("Application", () => {
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"));
+    fireEvent.click(getByText("Tuesday"));
 
-  return waitForElement(() => getByText("Monday"))
-    .then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
