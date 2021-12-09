3ad54c6c24cf66202791801020a46d9ff32ffc25
Neha Yadav <nehayadav903@gmail.com>
2021-12-09 13:32:34 -0500

    Create a test case for books an interview and reduces the spots remaining for the first day by 1

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -4,6 +4,8 @@ import {
   cleanup,
   waitForElement,
   fireEvent,
+  getByText,
+  prettyDOM,
 }
   from "@testing-library/react";
 import Application from "components/Application";
@@ -19,3 +21,23 @@ describe("Application", () => {
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 });
+it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+  const { container } = render(<Application />);
+  await waitForElement(() => queryByText(container, "Archie Cohen"));
+  const appointments = getAllByTestId(container, "appointment");
+  const appointment = appointments[0];
+
+  fireEvent.click(getByAltText(appointment, "Add"));
+
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+    target: { value: "Lydia Miller-Jones" },
+  });
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+  fireEvent.click(getByText(appointment, "Save"));
+
+  console.log(prettyDOM(appointment));
+});
+
+
+
