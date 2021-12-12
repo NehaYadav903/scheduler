980482a0f5dfd8bc5135dcca3a9d0e665a6bb37f
Neha Yadav <nehayadav903@gmail.com>
2021-12-12 18:45:54 -0500

    Add more testcases for cancel and edit

--- cypress/integration/1-getting-started/navigation.spec.js
+++ cypress/integration/1-getting-started/navigation.spec.js
@@ -2,11 +2,39 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-  
+
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
       .should("have.class", "day-list__item--selected")
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .click({ force: true });
+  
+    cy.contains("Confirm").click();
+  
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+  
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
   });
 });
