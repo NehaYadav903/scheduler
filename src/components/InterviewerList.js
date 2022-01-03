c002ef2da81657ebd9340a6c700f9c3ff02db709
Neha Yadav <nehayadav903@gmail.com>
2022-01-03 13:21:36 -0500

    Fix the error in interview list item

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
 
-
   // to show list of interviewers
   const interviewers = props.interviewers.map((interviewer) => {
     return (
